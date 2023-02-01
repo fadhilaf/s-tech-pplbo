dff9e45cf182e0a87a184dbe0deb3afdd8eb7141
FadhilAF <falfatih3421@gmail.com>
2023-02-01 10:44:13 +0700

    new branch

--- common/mysql/init.go
+++ common/mysql/init.go
@@ -4,21 +4,29 @@ import (
 	"database/sql"
 	"log"
 
+	_ "github.com/go-sql-driver/mysql"
+
   "github.com/golang-migrate/migrate/v4"
   "github.com/golang-migrate/migrate/v4/database/mysql"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func Start(mysqlConnection string) *sql.DB {
   db, err := sql.Open("mysql", mysqlConnection)
   if err != nil {
-    log.Fatalf("Error when connecting to database: %v", err)
+    log.Fatalf("Error when connecting to database (%v): %v", mysqlConnection , err)
   }
 
   driver, err := mysql.WithInstance(db, &mysql.Config{})
+  if err != nil {
+    log.Fatalf("Error when creating migration driver (%v): %v", mysqlConnection, err)
+  }
+
   m, err := migrate.NewWithDatabaseInstance(
-    "file:///common/mysql/migration", 
-    "postgres", driver )
+    "common/mysql/migration", 
+    "mysql", driver )
+  if err != nil {
+    log.Fatalf("Error when migration (%v): %v", mysqlConnection, err)
+  }
 
   m.Up() // or m.Step(2) if you want to explicitly set the number of migrations to run
 
