44021ec24db2b345a9d02c33962feca520e18a4f
Rendi Alamsyah <119906641+renn27@users.noreply.github.com>
2023-03-15 11:54:54 +0700

    Delete pplbo-fe/s-tech-pplbo directory
    hapus

--- pplbo-fe/s-tech-pplbo/common/postgres/migration/000001_init.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-  name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL UNIQUE,
-  password_hash varchar(255) NOT NULL,
-  address varchar(255) NOT NULL,
-  phone varchar(255) NOT NULL,
-  created_at timestamp NOT NULL DEFAULT (NOW())
-);
--- pplbo-fe/s-tech-pplbo/common/validation/register_custom_message.go
+++
@@ -1,7 +0,0 @@
-package validation
-
-func registerCustomMessages() {
-	customMessages["division_name"] = "Nama Divisi"
-	customMessages["id"] = "ID Divisi"
-	customMessages["year"] = "Tahun"
-}
