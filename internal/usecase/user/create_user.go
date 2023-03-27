0ebf2727eb200ff91e7a84dc32074e7fc87b44b1
FadhilAF <falfatih3421@gmail.com>
2023-03-27 14:53:50 +0700

    change password hash and check existing email order

--- internal/usecase/user/create_user.go
+++ internal/usecase/user/create_user.go
@@ -7,22 +7,23 @@ import (
 
 	"github.com/FadhilAF/s-tech-pplbo/internal/model"
 	repositoryModel "github.com/FadhilAF/s-tech-pplbo/internal/repository/postgres/sqlc"
+	"github.com/gin-gonic/gin"
 
 	utils "github.com/FadhilAF/s-tech-pplbo/internal/utils"
 )
 
 func (usecase *userUsecaseImpl) CreateUser(req model.CreateUserRequest) model.WebServiceResponse {
+	_, err := usecase.Store.GetUserByEmail(context.Background(), req.Email)
+	if err == nil {
+		return utils.ToWebServiceResponse("Email sudah terdaftar", http.StatusConflict, nil)
+	}
+
 	passwordHash, err := utils.HashPassword(req.Password)
 	if err != nil {
 		return utils.ToWebServiceResponse("Fungsi hash password gagal", http.StatusInternalServerError, nil)
 	}
 
-	_, err = usecase.Store.GetUserByEmail(context.Background(), req.Email)
-	if err == nil {
-		return utils.ToWebServiceResponse("Email sudah terdaftar", http.StatusConflict, nil)
-	}
-
-	_, err = usecase.Store.CreateUser(context.Background(), repositoryModel.CreateUserParams{
+	user, err := usecase.Store.CreateUser(context.Background(), repositoryModel.CreateUserParams{
 		Name:         req.Name,
 		Email:        req.Email,
 		PasswordHash: passwordHash,
@@ -34,5 +35,5 @@ func (usecase *userUsecaseImpl) CreateUser(req model.CreateUserRequest) model.We
 		return utils.ToWebServiceResponse("Menambah user ke db gagal", http.StatusInternalServerError, nil)
 	}
 
-	return utils.ToWebServiceResponse("User berhasil dibuat", http.StatusCreated, nil)
+	return utils.ToWebServiceResponse("User berhasil dibuat", http.StatusCreated, gin.H{"user": user})
 }
