eb686d5ab7cef2095803c4075c46f220b4366a4a
FadhilAF <falfatih3421@gmail.com>
2023-04-26 22:49:10 +0700

    simplify the drop type if exist

--- config/postgres/migration/20230322052407_create_order_table.down.sql
+++ config/postgres/migration/20230322052407_create_order_table.down.sql
@@ -2,10 +2,4 @@
 
 DROP TABLE IF EXISTS orders;
 
-DO $$
-BEGIN
-    IF EXISTS (SELECT 1 FROM pg_type WHERE typname = 'order_status') THEN
-        DROP TYPE order_status;
-    END IF;
-END $$;
-
+DROP TYPE IF EXISTS order_status;
